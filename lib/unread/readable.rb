635016cdd0ee5d4094a92d3f7dd246a64d89ccc4
Dennis Wu <dennissfwu@gmail.com>
2017-08-25 00:23:58 +0800

    Retrieve distinct records to avoid key duplicate

--- lib/unread/readable.rb
+++ lib/unread/readable.rb
@@ -22,9 +22,9 @@ module Unread
 
       def mark_ar_as_read(ar, reader)
         timestamp_field = readable_options[:on].to_sym
-        read_marks = ar.unread_by(reader).pluck(ar.klass.primary_key, timestamp_field).map do |mark|
+        read_marks = ar.unread_by(reader).distinct.pluck(ar.klass.primary_key, timestamp_field).map do |mark|
           mark += [ar.klass.base_class.name, reader.class.base_class.name, reader.id]
-        end
+        end.uniq
         if read_marks.present?
           columns = [:readable_id, :timestamp, :readable_type, :reader_type, :reader_id]
           ReadMark.import(columns, read_marks)
